6a36ca231cbbd38aa2c1206110b426a55a6591cd
Brandon LeBlanc <brandon@leblanc.codes>
2018-01-08 19:39:19 -0600

    set up appveyor

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("Collection of PowerShell utilities.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("LeBlanc Codes, LLC")]
+[assembly: AssemblyProduct("LeBlancCodes.PowerShell.Utilities")]
+[assembly: AssemblyCopyright("Copyright © LeBlanc Codes, LLC 2018")]
+[assembly: AssemblyTrademark("LeBlancCodes.PowerShell.Utilities")]
+[assembly: AssemblyCulture("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
